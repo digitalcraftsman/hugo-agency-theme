8f59eb0bcabc463534a1e722fd99951e280d5afa
digitalcraftsman <digitalcraftsman@users.noreply.github.com>
2015-06-18 19:44:12 +0200

    Added copyright note to script file

--- static/js/contact_me.js
+++ static/js/contact_me.js
@@ -1,3 +1,11 @@
+/*
+    This script was originally part of David Millers (https://github.com/davidtmiller)
+    Bootstrap theme "Freelancer" add some functionality to the contact form.
+
+    Licensed under the Apache License 2.0. For more information take a look at the orignal
+    repository unde https://github.com/IronSummitMedia/startbootstrap-freelancer.
+*/
+
 $(function() {
 
     $("input,textarea").jqBootstrapValidation({
