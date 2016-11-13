eeb86cd6cbcdda3fb5f2f02d191e3f2b80530c46
Michael Grosser <development@stp-ip.net>
2016-11-13 14:26:43 +0000

    Migrate jquery to v3.3.1
    Migrate smooth scrolling to jquery animate.
    
    Fixes: #51

--- static/js/agency.js
+++ static/js/agency.js
@@ -1,18 +1,22 @@
 /*!
- * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Based on Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+// Smooth scrolling via animate()
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
 });
 
 // Highlight the top nav as scrolling occurs
