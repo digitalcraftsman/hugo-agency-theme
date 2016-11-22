18f1682f0be5563b4b0de6e3475a2744bde8a55f
Michael Grosser <development@stp-ip.net>
2016-11-22 15:45:53 +0000

    Remove 3rd party scripts for scrolling magic

--- static/js/agency.js
+++ static/js/agency.js
@@ -7,7 +7,7 @@
 // Smooth scrolling via animate()
 $(document).ready(function(){
   $("a").on('click', function(event) {
-    if (this.hash !== "") {
+    if (this.hash && window.location.pathname == "/") {
       event.preventDefault();
       var hash = this.hash;
       $('html, body').animate({
@@ -17,6 +17,29 @@ $(document).ready(function(){
       });
     }
   });
+});
+
+// Navigation change on scroll
+$(document).ready(function(){
+  var maxOffset = 300;
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= maxOffset) {
+      $('.navbar-default').addClass('navbar-shrink');
+    }
+    else {
+      $('.navbar-default').removeClass('navbar-shrink');
+    }
+  });
+});
+
+$(document).ready(function(){
+  var maxOffset = 300;
+  if ($(window).scrollTop() >= maxOffset) {
+    $('.navbar-default').addClass('navbar-shrink');
+  }
+  else {
+    $('.navbar-default').removeClass('navbar-shrink');
+  }
 });
 
 // Highlight the top nav as scrolling occurs
